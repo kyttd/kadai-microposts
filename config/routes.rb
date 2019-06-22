93904fd6ef8441f880e7c3170d0407bf7703c7ac
kaito.tada <kuyt1992@gmail.com>
2019-06-22 14:13:02 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'microposts/create'
+  get 'microposts/destroy'
   root to: 'toppages#index'
 
   get 'login', to: 'sessions#new'
@@ -7,4 +9,6 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  
+  resources :microposts, only: [:create, :destroy]
 end
